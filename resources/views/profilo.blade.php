f15a25f124e8970de75b0c2a5e6da765ed6b3abe
tommaso.dare <tommaso.dare96@gmail.com>
2025-07-01 16:48:15 +0200

    Add navigation links for carrello and profilo

--- resources/views/profilo.blade.php
+++ resources/views/profilo.blade.php
@@ -1,25 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Home</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pagina del Profilo</title>
 </head>
-
 <body>
-    <h1>Welcome to Laravel Home Page</h1>
-    <p>This is a simple Laravel application.</p>
-
-    <p>{{$informazioni}}</p>
 
     <nav>
         <ul>
-            <li><a href="{{route("home")}}">Homepage</a></li>
+            <li><a href="{{route("home")}}">Home</a></li>
             <li><a href="{{route("carrello")}}">Carrello</a></li>
             <li><a href="{{route("profilo")}}">Profilo</a></li>
         </ul>
     </nav>
+    
 </body>
-
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,16 @@ Route::get('/', function () {
     $informazioni = "Questo è il mio primo progetto in Laravel!";
 
     return view('home', compact('informazioni'));
-});
+})->name('home');
 
 Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/carrello', function () {
+    return view('carrello');
+})->name('carrello');
+
+Route::get('/profilo', function () {
+    return view('profilo');
+})->name('profilo');
